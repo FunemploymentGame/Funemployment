30c92c6c1b9fd92ac8b323ef3dd1a09d84455813
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-02 16:09:02 -0700

    Added .gitignore.

--- Funemployment/Funemployment/Controllers/ProfileController.cs
+++ Funemployment/Funemployment/Controllers/ProfileController.cs
@@ -8,38 +8,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Funemployment.Controllers
 {
-    public class ProfileController : Controller
+  public class ProfileController : Controller
+  {
+
+    private FunemploymentDbContext _context;
+
+    public ProfileController(FunemploymentDbContext context)
     {
-        
-        private FunemploymentDbContext _context;
-
-        public ProfileController(FunemploymentDbContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            // add logic to check db for same username
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(Player player)
-        {
-            await _context.PlayerTable.AddAsync(player);
-            await _context.SaveChangesAsync();
-            return View();
-        }
-
-        
-
-
+      _context = context;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      // Add logic to check Db for same username
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Player player)
+    {
+      await _context.PlayerTable.AddAsync(player);
+      await _context.SaveChangesAsync();
+      return View();
+    }
+
+
+
+
+  }
 }
