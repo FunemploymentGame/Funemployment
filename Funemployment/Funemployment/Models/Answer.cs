620f1a960cf237a853c6529635fc4c06ba75d86e
Anthony Green <Anthonydgreen90@gmail.com>
2018-07-02 12:40:45 -0700

    Initial Commit: Todo- Add Entity framework core and SQL db

---
+++ Funemployment/Funemployment/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Funemployment.Models
+{
+    public class Answer
+    {
+        public int ID { get; set; }
+        public int QID { get; set; }
+        public int PID { get; set; }
+        public string Content { get; set; }
+    }
+}
