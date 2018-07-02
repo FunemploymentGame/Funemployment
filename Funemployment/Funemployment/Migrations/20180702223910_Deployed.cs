4b0d54e34d33244d0b453fe13b019afaf8ae0da7
Jermaine Walker <jermainemwalker@gmail.com>
2018-07-02 15:43:58 -0700

    Added ProductionConnection to Startup.cs.

---
+++ Funemployment/Funemployment/Migrations/20180702223910_Deployed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Funemployment.Migrations
+{
+    public partial class Deployed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Funemployment/Funemployment/Startup.cs
+++ Funemployment/Funemployment/Startup.cs
@@ -12,42 +12,45 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Funemployment
 {
-    public class Startup
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddDbContext<FunemploymentDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                name: "default",
-                template: "{controller=Home}/{action=Index}/{id?}");
-            });
-            app.UseStaticFiles();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      Configuration = configuration;
     }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      //services.AddDbContext<FunemploymentDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddDbContext<FunemploymentDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+              name: "default",
+              template: "{controller=Home}/{action=Index}/{id?}");
+      });
+      app.UseStaticFiles();
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello World!");
+      });
+    }
+  }
 }
